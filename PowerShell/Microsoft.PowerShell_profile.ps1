545f06dc74d76090941ebc0f0fcb3c714f643569
markus <me@markuskonrad.de>
2019-11-11 14:54:43 +0100

    Added config folders

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+# Open the file by typing "notepad $PROFILE" in PowerShell
+# Sets e.g. startup actions and imports for  each PowerShell window
+
+# Import posh-git to get git integration in the prompt
+Import-Module posh-git
+
+# Import oh-my-posh to beautify the prompt
+Import-Module oh-my-posh
+
+# Set Theme for oh-my-posh (https://github.com/JanDeDobbeleer/oh-my-posh#themes)
+Set-Theme Paradox
